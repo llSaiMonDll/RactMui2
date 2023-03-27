a7f38007fe0b4a96a1bc596599cccff81b8eddb3
llSaiMonDll <zerotwotv@gmail.com>
2023-03-27 14:49:01 +0700

    Finish

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./components/Home";
+import Todo from "./components/Todo";
+import TodoPage from "./pages/TodoPage";
+import "./App.css";
+function App(){
+    return (
+        <TodoPage/>
+        
+    )
+}
+export default App;
