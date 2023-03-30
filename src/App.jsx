e7ca2c3819e936c3860461979a8fe0f79a5a9de8
llSaiMonDll <zerotwotv@gmail.com>
2023-03-30 12:34:13 +0700

    Lab4-Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from "react";
-import Home from "./components/Home";
-import Todo from "./components/Todo";
+import { Route, Routes } from 'react-router-dom'
+import Calendar from "./pages/Calendar";
+import HomePage from "./pages/Homepage";
+import TodayPage from "./pages/Todaypage";
 import TodoPage from "./pages/TodoPage";
-import "./App.css";
-function App(){
-    return (
-        <TodoPage/>
-        
-    )
+import Home from "./components/Home"
+function App() {
+  return (
+    <Routes>
+        <Route exect path="/" element={<HomePage/>}/>
+        <Route path="/todo" element={<TodoPage/>}/>
+        <Route path ="/today" element ={<TodayPage/>}/>
+        <Route path = "/calendar" element={<Calendar/>}/>
+    </Routes>
+  )
 }
-export default App;
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import App from "./App"
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+);
